b2a360ca65230002b870ce5c14ba1ee8dd66da59
Luke Imhoff <Kronic.Deth@gmail.com>
2015-08-29 14:25:40 -0500

    Add jquery

--- brunch-config.js
+++ brunch-config.js
@@ -50,7 +50,7 @@ exports.config = {
   plugins: {
     babel: {
       // Do not use ES6 compiler in vendor code
-      ignore: [/web\/static\/vendor/]
+      ignore: [/web\/static\/vendor/, /node_modules\/jquery/]
     }
   },
 
